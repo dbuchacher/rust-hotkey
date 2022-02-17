ff3650be5fc2b7c78da2073240fc5a63e5d5f555
dbuchacher <goodmorning911@protonmail.com>
2022-02-17 10:42:47 -0700

    second

--- src/kam.rs
+++ src/kam.rs
@@ -2,7 +2,7 @@ pub use windows::Win32::UI::Input::KeyboardAndMouse::*;
 pub use windows::Win32::UI::WindowsAndMessaging::*;
 pub use windows::Win32::Foundation::*;
 pub use std::mem::zeroed;
-use once_cell::sync::Lazy; // 1.3.1
+use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
 // adds functionality to VIRTUAL_KEY types
@@ -37,6 +37,7 @@ use std::sync::Mutex;
         }
     }
 
+
 // send logic: reduces duplicate code
     fn send_down_logic(v_key: VIRTUAL_KEY) {
         match v_key {
@@ -59,6 +60,7 @@ use std::sync::Mutex;
             _ => key_event(v_key, 0, KEYEVENTF_KEYUP)
         }
     }
+
 
 // sends a key event
 fn key_event(v_key: VIRTUAL_KEY, w_scan: u16, dw_flags: KEYBD_EVENT_FLAGS) {
@@ -110,86 +112,110 @@ pub fn mouse_event(mouse_data: MOUSEHOOKSTRUCTEX_MOUSE_DATA, dw_flags: MOUSE_EVE
     // call windows api to do the magic
     unsafe { SendInput(c_inputs, &mut p_inputs, c_bsize); }
 }
-    // match w_param.0 as u32 {
-    // WM_KEYDOWN => {
-    //     let c: u8 = MapVirtualKeyW((*s).vkCode, MAPVK_VK_TO_CHAR) as u8;
-    //     println!("{}", c as char);
-    // }
-    // _ => (),
-    // }
 
+/* later stuff maybe
 
-// this function is in a library file
-pub unsafe extern "system" fn kbd_hook(n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
+    && ((*keyboard_data).flags & LLKHF_INJECTED).0 == 0
+    will act like '$' modifier in auto hotkey
+    
+    without LLKHF_INJECTEED
+    WM_KEYDOWN => if swapped_key.0.0 == v_key {}
+        swap(VK_A, VK_B);
+        swap(vk_B, VK_C);
+            will send 'c' when you press 'a'
 
+    with LLKHF_INJECTEED
+    WM_KEYDOWN => if swapped_key.0.0 == v_key && ((*keyboard_data).flags & LLKHF_INJECTED).0 == 0 {}
+        swap(VK_A, VK_B);
+        swap(vk_B, VK_C);
+            will send 'b' when you press 'a'
+*/
 
-    // match w_param.0 as u32 {
-    //     WM_LBUTTONDOWN => {
-    //         let c: u8 = MapVirtualKeyW((*keyboard_data).vkCode, MAPVK_VK_TO_CHAR) as u8;
-    //         println!("{} --", c);
+// each time keyboard or mouse events occur they will pass though this hook
+pub unsafe extern "system" fn hook(n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
+    // windows data variables storing info about current flags
+    let keyboard_data: *mut KBDLLHOOKSTRUCT = l_param.0 as _;
+    let mouse_data: *mut MSLLHOOKSTRUCT = l_param.0 as _;
+    let v_key = MapVirtualKeyW((*keyboard_data).vkCode, MAPVK_VK_TO_CHAR) as u16;
+
+    // clone global variables to prevent freeze up when looping them
+    let swapped_keys = SWAPKEYS.lock().unwrap().clone();
+    let blocked_keys = BLOCKKEYS.lock().unwrap().clone();
+
+    // for hotkey in HOTKEYS.lock().unwrap().iter() {
+    //     // check hotkey
+    //     let early_return = hotkey(n_code, w_param, l_param);
+
+    //     // if a hotkey condition is met we will return early
+    //     if early_return != CallNextHookEx(None, n_code, w_param, l_param) {
+    //         return early_return
     //     }
-    //     _ => (),
-    //     }
+    // }
 
-    for hotkey in HOTKEYS.lock().unwrap().iter() {
-        // check hotkey
-        let early_return = hotkey(n_code, w_param, l_param);
-
-        // if a hotkey contition is met we will return early
-        if early_return != CallNextHookEx(None, n_code, w_param, l_param) {
-            return early_return
+    for swapped_key in swapped_keys {
+        match w_param.0 as u32 {
+            WM_KEYDOWN => if swapped_key.0.0 == v_key && ((*keyboard_data).flags & LLKHF_INJECTED).0 == 0 { 
+                swapped_key.1.send(); return LRESULT(1)
+            },
+            _ => (),
+        }
+    }
+    
+    for blocked_key in blocked_keys {
+        match w_param.0 as u32 {
+            WM_KEYDOWN     => if blocked_key.0 == v_key        { return LRESULT(1) },
+            WM_KEYUP       => if blocked_key.0 == v_key        { return LRESULT(1) },
+            WM_LBUTTONDOWN => if blocked_key == VIRTUAL_KEY(1) { return LRESULT(1) },
+            WM_LBUTTONUP   => if blocked_key == VIRTUAL_KEY(1) { return LRESULT(1) },
+            WM_RBUTTONDOWN => if blocked_key == VIRTUAL_KEY(2) { return LRESULT(1) },
+            WM_RBUTTONUP   => if blocked_key == VIRTUAL_KEY(2) { return LRESULT(1) },
+            WM_MBUTTONDOWN => if blocked_key == VIRTUAL_KEY(4) { return LRESULT(1) },
+            WM_MBUTTONUP   => if blocked_key == VIRTUAL_KEY(4) { return LRESULT(1) },
+            WM_XBUTTONDOWN => if blocked_key == VIRTUAL_KEY(5) { return LRESULT(1) },
+            WM_XBUTTONUP   => if blocked_key == VIRTUAL_KEY(5) { return LRESULT(1) },
+            _ => (),
         }
     }
 
     CallNextHookEx(None, n_code, w_param, l_param)
 }
 
+// set hooks to monitor keyboard and mouse events
 pub unsafe fn set_hook() {
 
     // easy reading of 'SetWindowsHookExW' variables
-    let id_hook: WINDOWS_HOOK_ID = WH_KEYBOARD_LL;
-    let lpfn: HOOKPROC = Some(kbd_hook);
+    let id_hook_keyboard: WINDOWS_HOOK_ID = WH_KEYBOARD_LL;
+    let id_hook_mouse: WINDOWS_HOOK_ID = WH_MOUSE_LL;
+    let lpfn: HOOKPROC = Some(hook);
     let hmod: HINSTANCE = zeroed();
     let dw_thread_id: u32 = 0;
 
     //  installs hook to monitor keyboard events
-    let kbd: HHOOK = SetWindowsHookExW(WH_KEYBOARD_LL, lpfn, hmod, dw_thread_id);
-    let mouse: HHOOK = SetWindowsHookExW(WH_MOUSE_LL, lpfn, hmod, dw_thread_id);
+    let keyboard: HHOOK = SetWindowsHookExW(id_hook_keyboard, lpfn, hmod, dw_thread_id);
+    let mouse: HHOOK = SetWindowsHookExW(id_hook_mouse, lpfn, hmod, dw_thread_id);
 
     // message loop
     let mut message: MSG = zeroed();
     GetMessageW(&mut message, None, 0, 0);
-    UnhookWindowsHookEx(kbd);
+
+    // not used yet
+    UnhookWindowsHookEx(keyboard);
     UnhookWindowsHookEx(mouse);
 }
 
-pub fn add_hotkey(hotkey_function: fn(n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT) {
+
+// allows use of a gobal variable 'HOTKEYS' (a vector of functions)
+// using this to create different functions for each hotkey
+pub static HOTKEYS: Lazy<Mutex<Vec<unsafe fn (n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT>>> = Lazy::new(|| Mutex::new(vec![]));
+
+// block keys
+pub static BLOCKKEYS: Lazy<Mutex<Vec<VIRTUAL_KEY>>> = Lazy::new(|| Mutex::new(vec![]));
+
+// swap keys
+pub static SWAPKEYS: Lazy<Mutex<Vec<(VIRTUAL_KEY, VIRTUAL_KEY)>>> = Lazy::new(|| Mutex::new(vec![]));
+
+// use add_hotkey in 'main' to add a functions to 'HOTKEYS'
+// these will be checked in the hook message loop
+pub fn add_hotkey(hotkey_function: unsafe fn(n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT) {
     HOTKEYS.lock().unwrap().push(hotkey_function);
 }
-
-pub static HOTKEYS: Lazy<Mutex<Vec<fn (n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT>>> = Lazy::new(|| Mutex::new(vec![]));
-
-// for hotkey in HOTKEYS.lock().unwrap().iter() {
-//     hotkey()
-// }
-
-
-    // match w_param.0 as u32 {
-    // WM_KEYDOWN => {
-    //     let c: u8 = MapVirtualKeyW((*keyboard_data).vkCode, MAPVK_VK_TO_CHAR) as u8;
-    //     println!("{}", c);
-    // }
-    // _ => (),
-    // }
-
-// WM_LBUTTONDOWN
-// WM_RBUTTONDOWN
-// WM_MBUTTONDOWN
-// WM_XBUTTONDOWN
-
-// back(w_param, l_param);
-
-// fn back(w_param: WPARAM, l_param: LPARAM) {
-
-    // let keyboard_data: *mut KBDLLHOOKSTRUCT = l_param.0 as _;
-    // let mouse_data: *mut MSLLHOOKSTRUCT = l_param.0 as _;
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,107 @@
 use ahk::*;
 
 fn main() {
-    // HOTKEYS.lock().unwrap().push(a);
+    
+    swap(VK_A, VK_H);
+    swap(VK_H, VK_D);
 
-    add_hotkey(a);
-
+    block(VK_B);
+    
     unsafe { set_hook(); }
 }
 
-fn stop_a_key() {
+fn block(v_key: VIRTUAL_KEY) {
+    BLOCKKEYS.lock().unwrap().push(v_key);
+}
+fn swap(in_key: VIRTUAL_KEY, out_key: VIRTUAL_KEY) {
+    SWAPKEYS.lock().unwrap().push((in_key, out_key));
+}
 
+unsafe fn test(v_key: VIRTUAL_KEY, n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
+    let keyboard_data: *mut KBDLLHOOKSTRUCT = l_param.0 as _;
+    let mouse_data: *mut MSLLHOOKSTRUCT = l_param.0 as _;
+
+    match w_param.0 as u32 {
+    WM_KEYDOWN => {
+        let c: u8 = MapVirtualKeyW((*keyboard_data).vkCode, MAPVK_VK_TO_CHAR) as u8;
+        println!("{}", c);
+    }
+    _ => (),
+    }
+
+    CallNextHookEx(None, n_code, w_param, l_param)
+}
+
+// unsafe fn test(n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
+//     let keyboard_data: *mut KBDLLHOOKSTRUCT = l_param.0 as _;
+//     let mouse_data: *mut MSLLHOOKSTRUCT = l_param.0 as _;
+
+//     match w_param.0 as u32 {
+//     WM_KEYDOWN => {
+//         let c: u8 = MapVirtualKeyW((*keyboard_data).vkCode, MAPVK_VK_TO_CHAR) as u8;
+//         println!("{}", c);
+//     }
+//     _ => (),
+//     }
+
+//     CallNextHookEx(None, n_code, w_param, l_param)
+// }
+
+
+
+
+
+
+
+
+fn stop_a_key() {
     // if (*s).vkCode == 65 && VK_B.is_down() == true {
     //     println!("b + a was pressed");
     //     return LRESULT(1);        
     // }
-
 }
 
-fn a(n_code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-    unsafe {
-        let keyboard_data: *mut KBDLLHOOKSTRUCT = l_param.0 as _;
-        // let mouse_data: *mut MSLLHOOKSTRUCT = l_param.0 as _;
+// match w_param.0 as u32 {
+// WM_KEYDOWN => {
+//     let c: u8 = MapVirtualKeyW((*keyboard_data).vkCode, MAPVK_VK_TO_CHAR) as u8;
+//     println!("{}", c);
+// }
+// _ => (),
+// }
 
-        if (*keyboard_data).vkCode == 65 && VK_B.is_down() == true {
-            println!("b + a was pressed");
-            return LRESULT(1)
-        }
+// WM_LBUTTONDOWN
+// WM_RBUTTONDOWN
+// WM_MBUTTONDOWN
+// WM_XBUTTONDOWN
 
-        CallNextHookEx(None, n_code, w_param, l_param)
-    }
+// let keyboard_data: *mut KBDLLHOOKSTRUCT = l_param.0 as _;
+// let mouse_data: *mut MSLLHOOKSTRUCT = l_param.0 as _;
 
+// unsafe {
+//     HOTKEYS.lock().unwrap().push(
+//         |n_code: i32, w_param: WPARAM, l_param: LPARAM| -> LRESULT { 
+//             let keyboard_data: *mut KBDLLHOOKSTRUCT = l_param.0 as _;
+//             let mouse_data: *mut MSLLHOOKSTRUCT = l_param.0 as _;
+        
+//             // println!("{:?}", (*mouse_data));
+//             // println!("{:?}", (*keyboard_data));
+        
+        
+//             match w_param.0 as u32 {
+//                 WM_LBUTTONDOWN => {
+//                 let c: u8 = MapVirtualKeyW((*keyboard_data).vkCode, MAPVK_VK_TO_CHAR) as u8;
+//                 println!("{}", c);
+//             }
+//             _ => (),
+//             }
+        
+        
+//             if (*keyboard_data).vkCode == 65 && VK_B.is_down() == true {
+//                 println!("b + a was pressed");
+//                 return LRESULT(1)
+//             }
+//             CallNextHookEx(None, n_code, w_param, l_param)
+//         }
+//     );
+// }
 
-  //  println!("a {}", (*keyboard_data).vkCode );
-}
-
-
-fn b() {
-    println!("b");
-}
